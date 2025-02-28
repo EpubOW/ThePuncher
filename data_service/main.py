03d1cf94b0ada7d77fa510f3a37c032a67812165
I4Rt <n.tes.free@gmail.com>
2025-02-28 19:10:17 +0700

    Env scanning created; Dockerfile for data_service and docker-compose.yml created;

--- data_service/main.py
+++ data_service/main.py
@@ -4,11 +4,11 @@ from loguru import logger
 # from data_processing.MainProcessing import MainProcessing
 
 if __name__ == '__main__':
-    # app.start_app()
-    try:
-        while True:
-            print(puncher_client.read_data())
-            time.sleep(0.5)
-    except KeyboardInterrupt:
-        logger.warning('Interrupting...')
-        logger.info('Interrupted')
+    app.start_app()
+    # try:
+    #     while True:
+    #         print(puncher_client.read_data())
+    #         time.sleep(0.5)
+    # except KeyboardInterrupt:
+    #     logger.warning('Interrupting...')
+    #     logger.info('Interrupted')
--- data_service/src/config.py
+++ data_service/src/config.py
@@ -1,13 +1,26 @@
+import os
+
+def read_env(
+    env_value_name:str, 
+    default:str|int|float,
+):
+    val = os.getenv(env_value_name)
+    if val is None:
+        return default
+    return type(default)(val)
+
+
 # api config
-API_HOST  = '0.0.0.0'
-API_PORT  = 8001
-API_DEBUG = True
+API_HOST  = read_env('API_HOST',  default='0.0.0.0')
+API_PORT  = read_env('API_PORT',  default=8000)
+API_DEBUG = read_env('API_DEBUG', default=True)
 
 # device config
-SERIAL_NAME     = 'COM18'
-SERIAL_BAUDRATE = 115200
-SERIAL_PARITY   = 'N'
-SERIAL_STOPBITS = 1
-SERIAL_BYTESIZE = 8
-SERIAL_TIMEOUT  = 1
+SERIAL_NAME     = read_env('SERIAL_NAME',     default='COM18')
+SERIAL_BAUDRATE = read_env('SERIAL_BAUDRATE', default=115200)
+SERIAL_PARITY   = read_env('SERIAL_PARITY',   default='N')
+SERIAL_STOPBITS = read_env('SERIAL_STOPBITS', default=1)
+SERIAL_BYTESIZE = read_env('SERIAL_BYTESIZE', default=8)
+SERIAL_TIMEOUT  = read_env('SERIAL_TIMEOUT',  default=1)
 
+print(API_HOST, API_PORT, API_DEBUG)
